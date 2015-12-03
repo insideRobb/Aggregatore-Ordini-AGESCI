9157e6bcf4e2dc4fe8c2d2431b38a6b136330ed4
alessandro308 <alessandropagiaro@gmail.com>
2015-12-03 23:22:02 +0100

    config Sample

---
+++ mail_config-sample.php
@@ -0,0 +1,18 @@
+<?php
+	$your_name="Ordini Agesci";
+	$my_email="your-mail"; //your-email address
+	$ricevutaDir="http://your-domain.com/ricevuta";
+	$useSMTP=true; //use true if use SMTP, false use php mail()
+	$username="email-user";
+	$password="email-psw";
+	
+	$host = "smtp.gmail.com";
+	$port = 587;
+	
+	$iban = "IBAN";
+	$propIban = "Proprietario Iban";
+	$costoGestioneOrdine = 0.50; //Costo da aggiungere fisso ad ogni ordine per coprire spese di spedizione
+	//change password into ArticoliDb.php to Database Open
+
+	//RICHIEDE CONNESSIONE SSL!!
+?>
